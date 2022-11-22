9c69c3317b0cf2a82f727320a913b0f7307a2680
ldvermunt <ldvermunt@outlook.com>
2022-11-22 12:47:04 -0500

    commit multiple markers

--- Mapping_Multiple_Points/Static/js/logic.js
+++ Mapping_Multiple_Points/Static/js/logic.js
@@ -4,7 +4,7 @@ console.log("working");
 // Create the map object with a center and zoom level.  This particulat setup is for the geographic center of tha USA
 //2 examples of how to write the code are below, first one hashed out.
 
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 5);
 //let map = L.map("mapid", {
 //    center: [
 //      40.7, -94.5
@@ -12,18 +12,32 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 //    zoom: 4
 //  });
 
-//Add a marker to the map
-//var marker = L.marker([51.5, -0.09]).addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Get data from cities.js (adjacent folder)
+let cityData = cities;
 
-// Add a circle marker to the map - Black border, yellow inside, 300 meter radius.
-let marker = L.circle([34.0522, -118.2437],{
-    radius: 300,
-    color: "Black",
-    fillColor: "yellow"
-}).addTo(map);
+  //Loop through the cities array (in adjacent folder) and log them to the console
+  cityData.forEach(function(city) {
+    console.log(city)
+
+   //add a variable for city.populaiton to subtract 200000 people (per skill drill)
+   
+    var newpop = city.population-200000
+  
+ //Add a marker for each cities location.  Tie city population to circle marker size.  Add a popup for population and city name.
+
+
+ L.circleMarker(city.location,
+    {radius: city.population/100000,
+    color: "yellow"
+})
+  .bindPopup("<h2>" + city.city + "," + city.state + "</h2> <hr> <h3>Population " + newpop.toLocaleString() + "</h3>")
+    .addTo(map);
+  });
+
+
+
+
 
 // We create the tile layer that will be the background of our map.  
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
